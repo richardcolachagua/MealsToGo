cb4eb5c48f4e96c153ecfc4d23ed8df6d58271f1
Richard Colachagua <Richardcolachagua@gmail.com>
2022-10-19 08:24:17 -0400

    adding info-card & other features

---
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -0,0 +1,87 @@
+import React from "react";
+import styled from "styled-components/native";
+import { Image } from "react-native";
+import { Card } from "react-native-paper";
+import { SvgXml } from "react-native-svg";
+import { Spacer } from "./spacer/spaceer.component";
+import { Text } from "./typography";
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
+
+const RestaurantCard = styled(Card)`
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const RestaurantCardCover = styled(Card)`
+  padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+
+const Address = styled(Text)`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSizes.caption};
+`;
+
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
+const Section = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const SectionEnd = styled.View`
+  flex: 1;
+  flex-direction: row;
+  align-items: flex-end;
+`;
+
+export const RestaurantInfoCard = ({ restaurant = {} }) => {
+  const {
+    name = "Some Restaurant",
+    icon,
+    photos = [
+      "https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg",
+    ],
+    address = "100 some random street",
+    isOpenNow = true,
+    rating = 4,
+    isClosedTemporarily,
+  } = restaurant;
+
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+  console.log(ratingArray);
+
+  return (
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+      <Info>
+        <Text variant="label">{name}</Text>
+        <Section>
+          <Rating>
+            {ratingArray.map(() => (
+              <SvgXml xml={star} width={20} height={20} />
+            ))}
+          </Rating>
+          <SectionEnd>
+            {isClosedTemporarily && (
+              <Text variant="caption">CLOSED TEMPORARILY</Text>
+            )}
+            <Spacer positon="left" size="large" />
+            {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+            <Spacer positon="left" size="large" />
+            <Image style={{ width: 15, height: 15 }} source={{ img: icon }} />
+          </SectionEnd>
+        </Section>
+        <Address>{address}</Address>
+      </Info>
+    </RestaurantCard>
+  );
+};
